1fa065d9b66009dd6662d972016ef91a02a05360
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-05-08 20:55:18 -0300

    refactor: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
